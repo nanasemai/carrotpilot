a9038241199bae4fa5f6a6ab63d33f2fa097162e
nanasemai <42890263+nanasemai@users.noreply.github.com>
2025-12-21 13:43:46 +0800

    PC init AMD GPU

--- selfdrive/modeld/modeld.py
+++ selfdrive/modeld/modeld.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import os
 from openpilot.system.hardware import TICI
-os.environ['DEV'] = 'QCOM' if TICI else 'LLVM'
+os.environ['DEV'] = 'QCOM' if TICI else 'AMD'  # 默认使用AMD GPU
 USBGPU = "USBGPU" in os.environ
 if USBGPU:
   os.environ['DEV'] = 'AMD'
@@ -314,7 +314,7 @@ def main(demo=False):
       lat_smooth_seconds = params.get_float("LatSmoothSec") * 0.01
       long_delay = params.get_float("LongActuatorDelay")*0.01
       vEgoStopping = params.get_float("VEgoStopping") * 0.01
-      
+
     if custom_lat_delay > 0.0:
       lat_delay = custom_lat_delay + lat_smooth_seconds
     else:
