c309e8681ab716c1da12ae1b853744c03cfbe2d0
nanasemai <42890263+nanasemai@users.noreply.github.com>
2026-04-12 17:35:31 +0800

    Fix hardware info display on x86_64 Ubuntu

--- selfdrive/ui/qt/screenrecorder/screenrecorder.h
+++ selfdrive/ui/qt/screenrecorder/screenrecorder.h
@@ -7,7 +7,7 @@
 #include <thread>
 #include <chrono>
 
-#ifdef WSL2
+#if defined(WSL2) || defined(X86_64)
 class ScreenRecoder : public QPushButton {
 public:
     ScreenRecoder(QWidget* parent = nullptr) {}
